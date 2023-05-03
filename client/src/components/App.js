053fbc98d6d598139c4e70192dd581b3115893ee
Timothy Jensen <timothy.jensen.13@gmail.com>
2023-05-03 14:03:32 -0400

    functional frontend

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,7 @@
-import { useEffect } from 'react'
-import Authentication from './components/Authentication'
+import React, {useState, useEffect} from 'react'
+import {Switch, Route, useHistory} from 'react-router-dom'
+import Home from './Home'
+// import Authentication from './components/Authentication'
 
 function App() {
     const [user, setUser] = useState(null)
@@ -9,32 +11,33 @@ function App() {
     
 
 
-    const fetchUser = () = {
+    // const fetchUser = () = {
 
-    }
+    // }
 
 
     const updateUser = (user) => setUser(user)
 
     return (
-        <>
-        <GlobalStyle/>
-        <Navigation updateUser={updateUser}/>
-            <Switch>
+        <div className ='navbar'>
+            <h1 className ='navbarh'>The Vault</h1>
+    {/* //     <GlobalStyle/>
+    //     <Navigation updateUser={updateUser}/>
+    //         <Switch>*/}
                 <Route exact path='/'>
                     <Home/>
                 </Route>
-                <Route exact path='/collections'>
-                    <Collections/>
-                </Route>
-                <Route path='/collections'>
-                    <Collections/>
-                </Route>                
-                <Route exact path='/authentication'>
-                    <Authentication updateUser={updateUser}/>
-                </Route>
-            </Switch>
-        </>
+                 {/* <Route exact path='/collections'>
+                     <Collections/>
+                 </Route>
+                 <Route path='/collections'>
+                     <Collections/>
+                 </Route>                
+                 <Route exact path='/authentication'>
+                     <Authentication updateUser={updateUser}/>
+                 </Route>
+             </Switch> */}
+        </div>
     )
 }
 
